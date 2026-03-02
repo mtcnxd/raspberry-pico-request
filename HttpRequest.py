cc83e3b2a74620ae42d618e9afb213c1b356eebd
Marcos Tzuc Cén <mtc.nxd@gmail.com>
2026-03-02 17:44:36 -0600

    Refactor Request class and add getDateTime method
    Refactor Request class to initialize with base URL and add getDateTime method.

--- HttpRequest.py
+++ HttpRequest.py
@@ -3,8 +3,11 @@ import requests
 import json
 
 class Request:
-    def send():
-        url = "http://api.openweathermap.org/data/2.5/weather?q=merida&appid=192dbed2ac66d17d5f75780635e474fa"
+    def __init__():
+        self.baseUrl="https://mecanicarubio.com/api"
+    
+    def getDateTime():
+        url = f"{self.baseUrl}/sensors/time"
         response = requests.get(url)
         return json.loads(response.text)
 
